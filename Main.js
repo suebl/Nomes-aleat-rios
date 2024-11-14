6e200e7c207ebb4ee0086f3489c6e4d14d00ba52
suebl <108080683+suebl@users.noreply.github.com>
2024-11-14 12:15:50 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,35 @@
+JS
+
+const nomes = [“Ana”, “Fernanda”, “Maria Eduarda”,
+“Marcelo”, “Amanda”, “Gustavo”, “Gabriel”];
+
+const nomes = [“Ana”, “Fernanda”, “Maria Eduarda”,
+“Marcelo”, “Amanda”, “Gustavo”, “Gabriel”];
+
+export function aleatorio (lista){
+const posicao = Math.floor(Math.random()* lista.length);
+return lista[posicao];
+}
+export const nome = aleatorio(nomes);
+
+import { aleatorio, nome } from ‘./aleatorio.js’;
+
+function mostraResultado() {
+caixaPerguntas.textContent = `Em 2049, ${nome}`;
+
+function substituiNome() {
+for (const pergunta of perguntas) {
+pergunta.enunciado = pergunta.enunciado.replace(/você/g,
+nome);
+}
+}
+
+function substituiNome() {
+for (const pergunta of perguntas) {
+pergunta.enunciado = pergunta.enunciado.replace(/você/g,
+nome);
+}
+}
+substituiNome();
+mostraPergunta();
 
